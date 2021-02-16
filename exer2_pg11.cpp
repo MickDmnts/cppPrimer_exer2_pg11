cd36aedbccc3bf1a7160c0578b22ad3763c5e8bc
Michael Evangelos Diamantis <michaeldiamantis17@outlook.com>
2021-02-16 04:57:30 +0200

    Completed the exercise.

--- exer2_pg11.cpp
+++ exer2_pg11.cpp
@@ -5,19 +5,32 @@
 *and LegalOutputStatements().
 */
 
-void NestedComments()
+void NestedComments() //Task 1.7
 {
+//	/*
+//	* This is an /* incorrectly nested comment.*/
+//	* Everything below this point is considered source code.
+//	* I wont compile.
+//	* Remove the "//" before each line to see the effect.
+//	*/
 
+	std::cout << "See source code to view the error of this nested comment." << std::endl;
 }
 
-void LegalOutputStatements()
+void LegalOutputStatements() //Task 1.8
 {
-
+	std::cout << "/* is legal" << std::endl;
+	std::cout << "*/ is legal" << std::endl;
+	//std::cout << /* "*/" */<< std::endl; //throws error
+	std::cout << /* "*/" /* is legal under conditions"/*" */ << std::endl; //throws error if the above statement statement doesn't get commented out
 }
 
 int main()
 {
+	NestedComments();
+	system("pause");
 
+	LegalOutputStatements();
 
 	return 0;
 }
